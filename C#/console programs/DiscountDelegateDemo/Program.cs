a22b367e0e7515927629262ed9dffdeb5f9fbfdd
DamianMHayes <142570511+DamianMHayes@users.noreply.github.com>
2023-08-23 11:55:19 -0400

    Add files via upload

---
+++ C#/console programs/DiscountDelegateDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static System.Console;
+delegate void DiscountHandler(ref double saleAmount);
+
+class DiscountDelegateDemo{
+    public static void StandardDiscount(ref double saleAmount)
+    {
+        const double DISCOUNT_RATE = 0.05;
+        const double CUTOFF = 1000.0;
+        double discount;
+
+        if(saleAmount >= CUTOFF)
+        {
+            discount = saleAmount*DISCOUNT_RATE;
+        }
+        else
+        {
+            discount = 0;
+        }
+        saleAmount -= discount;
+    }
+
+    public static void PreferredDiscount(ref double saleAmount)
+    {
+        const double SPECIAL_DISCOUNT = 0.10;
+        double discount = saleAmount*SPECIAL_DISCOUNT;
+        saleAmount -= discount;
+    }
+
+    static void Main()
+    {
+        double saleAmount;
+        char code;
+        DiscountHandler firstDel, secondDel, thirdDel;
+        firstDel = new DiscountHandler(StandardDiscount);
+        secondDel = new DiscountHandler(PreferredDiscount);
+        thirdDel = firstDel;
+        thirdDel += secondDel;
+
+        Write("Enter amount of sale ");
+        saleAmount = Convert.ToDouble(ReadLine());
+        Write("Enter S for standard discount, or P for prefered discount,/nor X for eXtreme discount ");
+        code = Convert.ToChar(ReadLine());
+        if(code == 'S')
+        {
+            firstDel(ref saleAmount);
+        }
+        else if (code == 'P')
+        {
+            secondDel(ref saleAmount);
+        }
+        else
+        {
+            thirdDel(ref saleAmount);
+        }
+        WriteLine("New sale amount is {0}", saleAmount.ToString("C2"));
+    }
+}
---
+++ C#/console programs/fileTesting/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using static System.Console;
+using System.IO;
+class fileTest {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        
+        if (File.Exists("Program.cs"))
+        {
+            Console.WriteLine("File exists");
+        }
+        else
+        {
+            Console.WriteLine("File not found");
+        }
+    }
+}
+
+
+
+
