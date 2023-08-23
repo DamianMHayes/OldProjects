8c189a334a31115899e6b3aa56c160311f3f4c49
DamianMHayes <142570511+DamianMHayes@users.noreply.github.com>
2023-08-23 12:06:44 -0400

    Add files via upload

---
+++ C#/console programs/ReadNameFile/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using static System.Console;
+using System.IO;
+
+class ReadNameFile
+{
+    static void Main()
+    {
+        FileStream file = new FileStream("Names.txt", FileMode.Open, FileAccess.Read);
+        StreamReader reader = new StreamReader(file);
+        int count = 1;
+        string name;
+
+        WriteLine("Displaying all names");
+        name = reader.ReadLine();
+        while(name != null)
+        {
+            WriteLine("" + count + " " + name);
+            name = reader.ReadLine();
+            count++;
+        }
+        reader.Close();
+        file.Close();
+    }
+}
